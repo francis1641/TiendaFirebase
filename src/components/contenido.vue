0dfee7f99a1d5f7a598850398b2d5bb96c4052bf
francis1641 <francis1641@users.noreply.github.com>
2021-06-07 10:08:56 +0200

    Update woork

--- src/components/contenido.vue
+++ src/components/contenido.vue
@@ -1,19 +1,47 @@
 <template>
+
+    <div>
+    <div>
+        <button v-if="!authenticated" @click="abrirSesion">Login</button>
+        <div v-if="authenticated">
+            <button @click="cerrarSesion">Logout</button>
+            <h1>Hi {{ firstName }}!</h1>
+        </div>
+    </div>  
     <div class="contenido">
         <card v-for="game in juegos" v-bind:juego="game" v-bind:key="game.id"/>
+      </div>
 
 
     </div>
 </template>
 <script>
 import { db } from '../db'
+import autentificacion from '../db'
 import card from './card.vue'
 
 export default {
   data() {
     return {
       juegos: [],
+      user:{
+        loggedIn: false,
+        data:{}
+      }
     }
+  },
+
+  mounted: function() {
+      autentificacion.auth.onAuthStateChanged( user => {
+        if (user) {
+          this.user.loggedIn = true;
+          this.user.data = user;
+        }
+        else {
+          this.user.loggedIn = false;
+          this.user.data = {};
+        }
+      })
   },
 
   components:{
@@ -23,6 +51,25 @@ export default {
   firestore: {
     juegos: db.collection('Games'),
   },
+  methods:{
+    abrirSesion(){
+        autentificacion.login();
+    },
+    cerrarSesion(){
+        autentificacion.logout()
+    }
+  },
+  computed:{
+    authenticated(){
+          return this.user.loggedIn
+        },
+    firstName(){
+      if (this.user.data.displayName) {
+          return this.user.data.displayName
+      }
+          return null
+        }
+  }
 }
 </script>
 
--- src/db.js
+++ src/db.js
@@ -1,10 +1,53 @@
 import firebase from 'firebase/app'
 import 'firebase/firestore'
+import 'firebase/auth'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAHOYdhQv7fl8LpNE_nd2hcVNvfWKNKTfg",
+  authDomain: "tiendafirebase-b7020.firebaseapp.com",
+  projectId: "tiendafirebase-b7020",
+  storageBucket: "tiendafirebase-b7020.appspot.com",
+  messagingSenderId: "360754765868",
+  appId: "1:360754765868:web:c8f662311ba0e63cffb189",
+  measurementId: "G-7QSGXCWC6B"
+};
 
 // Get a Firestore instance
 export const db = firebase
-  .initializeApp({ projectId: 'tiendafirebase-b7020' })
+  .initializeApp( firebaseConfig )
   .firestore()
+
+  export default {
+    auth: firebase.auth(),
+    login() {
+      const provider = new firebase.auth.GoogleAuthProvider();
+ /*     LOGIN CON FACEBOOK    NO FUNCIONA      MAL
+      var provider1 = new firebase.auth.FacebookAuthProvider();
+      firebase.auth().signInWithPopup(provider1)
+      .then(function(result) {
+        console.log(result);
+      })
+*/
+      firebase.auth().signInWithPopup(provider)
+      .then(function(result) {
+        console.log(result);
+      })
+      .catch(function(error){
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        const email = error.email;
+        const credential = error.credential;
+        console.log(errorCode, errorMessage, email, credential);
+        })
+    },
+    logout() {
+      firebase.auth().signOut()
+      .then(function() {})
+      .catch(function(error) {
+        console.log(error)});
+    }
+}
+
 
 // Export types that exists in Firestore
 // This is not always necessary, but it's used in other examples
